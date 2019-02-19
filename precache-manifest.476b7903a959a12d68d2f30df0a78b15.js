e38e3660a373f18156de9746cac2452d2bf2c5c2
Des <desinoelle@gmail.com>
2019-02-18 21:16:55 -0500

    Updates

---
+++ precache-manifest.476b7903a959a12d68d2f30df0a78b15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "48f659dc3002cd7d3aee",
+    "url": "/clickygame/static/js/main.48f659dc.chunk.js"
+  },
+  {
+    "revision": "5ea0de113446dc0a7829",
+    "url": "/clickygame/static/js/1.5ea0de11.chunk.js"
+  },
+  {
+    "revision": "48f659dc3002cd7d3aee",
+    "url": "/clickygame/static/css/main.36f85a50.chunk.css"
+  },
+  {
+    "revision": "d4bd1251481fbf0521aa1895009b9230",
+    "url": "/clickygame/index.html"
+  }
+];
